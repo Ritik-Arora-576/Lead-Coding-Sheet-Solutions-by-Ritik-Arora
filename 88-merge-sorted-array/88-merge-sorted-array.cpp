132412ed51d295cbd038f86f2e667fc5c0a7391a
Ritik Arora <68052449+Ritik-Arora-576@users.noreply.github.com>
2022-05-10 17:40:48 +0530

    Time: 3 ms (70.36%), Space: 9.1 MB (73.37%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,22 +1,18 @@
 // TC: O(m+n)
-// SC: O(m+n)
+// SC: O(1)
+// In place Algo
 
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> temp;
-        int i=0;
-        int j=0;
+        int k=m+n-1;
+        int i=m-1;
+        int j=n-1;
         
-        while(i<m and j<n)
+        while(j>=0)
         {
-            if(nums1[i]>nums2[j]) temp.push_back(nums2[j++]);
-            else temp.push_back(nums1[i++]);
+            if(i>=0 and nums1[i]>nums2[j]) nums1[k--]=nums1[i--];
+            else nums1[k--]=nums2[j--];
         }
-        
-        while(i<m) temp.push_back(nums1[i++]);
-        while(j<n) temp.push_back(nums2[j++]);
-        
-        nums1=temp;
     }
 };
