cd52c6f883b7fe341511bb8e71f1ff084007b376
Ritik Arora <68052449+Ritik-Arora-576@users.noreply.github.com>
2022-05-10 17:42:42 +0530

    Time: 8 ms (13.47%), Space: 9.1 MB (31.70%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -5,10 +5,12 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        // initializes pointer 'k' with the last index of nums1
         int k=m+n-1;
         int i=m-1;
         int j=n-1;
         
+        // overwrite the larger element between nums1 and nums2 at the index 'k'
         while(j>=0)
         {
             if(i>=0 and nums1[i]>nums2[j]) nums1[k--]=nums1[i--];
