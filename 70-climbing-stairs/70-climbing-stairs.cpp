54201d8ae42c073260be2558414b99015d0ad421
Ritik Arora <68052449+Ritik-Arora-576@users.noreply.github.com>
2022-05-11 20:44:39 +0530

    Time: 4 ms (20.44%), Space: 5.9 MB (56.39%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,21 @@
+// TC: O(N)
+// SC: O(1)
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=2) return n;
+        
+        int a=1;
+        int b=2;
+        
+        for(int i=2;i<n;i++)
+        {
+            int temp=a;
+            a=b;
+            b+=temp;
+        }
+        
+        return b;
+    }
+};
