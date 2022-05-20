1ddc2167477c5aabf735888d680e3d6850444d8b
Ritik Arora <68052449+Ritik-Arora-576@users.noreply.github.com>
2022-05-20 11:34:16 +0530

    Time: 33 ms (83.33%), Space: 7.4 MB (82.80%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -20,32 +20,34 @@ class Solution {
 public:
     // 2-D DP approach
     // TC: O(n*n*d)
-    // SC: O(n*d)
+    // SC: O(n)
     int minDifficulty(vector<int>& jobDifficulty, int d) {
         int n=jobDifficulty.size();
-        int dp[d+1][n+1];
+        int dp[2][n+1];
         
         dp[0][n]=0;
+        dp[1][n]=-1;
         
         for(int i=0;i<n;i++) dp[0][i]=-1;
-        for(int i=1;i<=d;i++) dp[i][n]=-1;
         
         for(int i=1;i<=d;i++)
         {
             for(int j=n-1;j>=0;j--)
             {
                 int maxEle=0;
-                dp[i][j]=INT_MAX;
+                dp[1][j]=INT_MAX;
                 for(int k=j;k<n;k++)
                 {
                     maxEle=max(maxEle,jobDifficulty[k]);
-                    if(dp[i-1][k+1]!=-1) dp[i][j]=min(dp[i][j],maxEle+dp[i-1][k+1]);
+                    if(dp[0][k+1]!=-1) dp[1][j]=min(dp[1][j],maxEle+dp[0][k+1]);
                 }
                 
-                if(dp[i][j]==INT_MAX) dp[i][j]=-1;
+                if(dp[1][j]==INT_MAX) dp[1][j]=-1;
             }
+            
+            for(int j=0;j<=n;j++) dp[0][j]=dp[1][j];
         }
         
-        return dp[d][0];
+        return dp[1][0];
     }
 };
