687ac327045b20ff9630cc51d559929c24d50e73
Ritik Arora <68052449+Ritik-Arora-576@users.noreply.github.com>
2022-07-16 20:55:26 +0530

    Time: 17 ms (89.30%), Space: 11.8 MB (99.86%) - LeetHub

--- 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.cpp
+++ 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.cpp
@@ -9,7 +9,11 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// Partition DP
+
 class Solution {
+    // recursive call
     vector<TreeNode*> store(int s,int e)
     {
         vector<TreeNode*> ans;
@@ -37,6 +41,43 @@ class Solution {
     
 public:
     vector<TreeNode*> generateTrees(int n) {
-        return store(1,n);
+        vector<TreeNode*> dp[n][n];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==j) dp[i][j]={new TreeNode(i+1)};
+                else if(i>j) dp[i][j]={NULL};
+                else dp[i][j]={};
+            }
+        }
+        
+        for(int j=1;j<n;j++)
+        {
+            for(int i=j-1;i>=0;i--)
+            {
+                for(int k=i;k<=j;k++)
+                {
+                    vector<TreeNode*> left,right;
+                    if(i>k-1) left={NULL};
+                    else left=dp[i][k-1];
+                    if(k+1>j) right={NULL};
+                    else right=dp[k+1][j];
+                    
+                    for(auto l:left)
+                    {
+                        for(auto r:right)
+                        {
+                            TreeNode* root=new TreeNode(k+1);
+                            root->left=l;
+                            root->right=r;
+                            dp[i][j].push_back(root);
+                        }
+                    }
+                }
+            }
+        }
+        
+        return dp[0][n-1];
     }
 };
